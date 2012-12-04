a44af84b579955afb34d5cf2fd47757458f86a67
joshuacelliott <joshuacelliott@gmail.com>
2012-12-03 23:35:46 -0700

    starting over...

--- apps/language/urls.py
+++
@@ -1,9 +0,0 @@
-from django.conf.urls.defaults import *
-
-urlpatterns = patterns('language.views',
-    url(r'^all/$', 'all_languages', name="language_all_languages"),
-    url(r'^$', 'user_languages', name="language_user_languages"),
-    url(r'^(?P<id>\d+)/$', 'language', name="language_language"),
-    url(r'^new/$', 'new_language', name="language_new_language"),
-    # url(r'^forge/(?P<id>\d+)/$', 'forge_language', name="language_forge_language"),
-)
